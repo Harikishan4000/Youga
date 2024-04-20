98316d17225532e89a3487c5fc46abbaaabb6da9
Harikishan M <harikishan4000@gmail.com>
2024-04-20 12:43:40 +0530

    Update app.py

--- Youga/flask/app.py
+++ Youga/flask/app.py
@@ -6,6 +6,8 @@ import numpy as np
 from time import time
 import mediapipe as mp
 import matplotlib.pyplot as plt
+import base64
+
 
 import functions as my_fc   ## User defined functions
 
@@ -23,11 +25,17 @@ camera=cv2.VideoCapture("http://192.168.1.4:8080/video")
 option=1
 #youtube - Stream live Video from Mobile phone Camera with Python and OPen CV
 #PlayStore - IP webcam
-label=""
-problems=[]
 
+oc=0
 def generate_frames():
+    counter=0
+    label=""
+    problems=[]
+    poseName=""
+
     while True:
+        counter+=1
+        landmarks=[]
         success, frame=camera.read()
 
         if not success:
@@ -38,12 +46,12 @@ def generate_frames():
             frame=cv2. flip(frame, 1)
             frame_height, frame_width, _ = frame.shape
             frame=cv2.resize(frame, (int(frame_width*(640/frame_height)), 640))
+            if counter==30:
+                frame, landmarks=my_fc.detectPose(frame, pose_vid, display=False)
 
-            frame, landmarks=my_fc.detectPose(frame, pose_vid, display=False)
-
-            if landmarks:
+            if landmarks and counter==30:
                 frame, label, problems, poseName, flag=my_fc.classifyPose(landmarks, frame, option, display=False)
-                    
+                counter=0    
             ret, buffer=cv2.imencode(".jpg", frame)
             frame=buffer.tobytes()
             
@@ -53,12 +61,17 @@ def generate_frames():
             # if cv2.waitKey(10) & 0xFF == ord('q'):
             #     break
 
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n'+frame+b'\r\n')
+        
+        # print("reached")
 
-        # yield (b'--frame\r\n'
-        #        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
-        #        b'Content-Type: text/plain\r\n\r\n' + label_bytes)
+        html_response = (
+                '<img src="data:image/jpeg;base64,' + base64.b64encode(frame).decode() + '">' +
+                '<div style="color: white">' + label + '</div>'
+            )
+
+        return html_response
+
+        
 
 
         
